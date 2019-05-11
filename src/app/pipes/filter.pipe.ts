9ccd2db44012aaae71716758f51ebbae0219658d
mbolanosc <mrybc96@gmail.com>
2019-05-11 11:50:43 -0600

    clean code

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -15,17 +15,17 @@ export class FilterPipe implements PipeTransform {
 
   transform(value: any, arg: any): any {
     /**Validates if the data comes empty */
-    if (arg === "" || arg.length < 2) { return value;}
+    if (arg === '' || arg.length < 2) { return value; }
 
     const resultReviews = [];
-    for (const review of value){
-      if (review.display_title.toLowerCase().indexOf(arg.toLowerCase()) > -1){
+    for (const review of value) {
+      if (review.display_title.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
       resultReviews.push(review);
-      } else if (review.byline.toLowerCase().indexOf(arg.toLowerCase()) > -1){
+      } else if (review.byline.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         resultReviews.push(review);
-      } else if (review.headline.toLowerCase().indexOf(arg.toLowerCase()) > -1){
+      } else if (review.headline.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         resultReviews.push(review);
-      } else if (review.summary_short.toLowerCase().indexOf(arg.toLowerCase()) > -1){
+      } else if (review.summary_short.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         resultReviews.push(review);
       }
     }
